30ad8a9fba0687fe0d1ff27f62f047e1e9cb9204
ftnext <takuyafjp+develop@gmail.com>
2023-07-17 15:43:53 +0900

    [bugfix] Pydantic V2 raises int_from_float error

--- chapter14/simple_model.py
+++ chapter14/simple_model.py
@@ -14,7 +14,13 @@ class Model2:
 
 if __name__ == "__main__":
     print(f'{Model(value="123")=}')  # type: ignore[arg-type]
-    print(f"{Model(value=0.5)=}")  # type: ignore[arg-type]
+
+    # Pydantic V2では int_from_float error
+    # https://docs.pydantic.dev/latest/usage/validation_errors/#int_from_float
+    try:
+        print(f"{Model(value=0.5)=}")  # type: ignore[arg-type]
+    except ValidationError:
+        pass
 
     try:
         Model2(value="0023")  # type: ignore[arg-type]
